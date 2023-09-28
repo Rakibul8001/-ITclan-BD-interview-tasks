85826ab0f19cad502fa635d7622da2508aca87ac
Rakibul8001 <ghumpori8001@gmail.com>
2023-09-29 01:29:44 +0600

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -99,7 +99,7 @@
 @push('scripts')
 <script>
 $(document).ready(function(){
-    alert('alert from home page');
+    // alert('alert from home page');
     function tournamentRefreshPage() {
         location.reload();
         console.log("welcome");
